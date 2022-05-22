e97c9a14b4c52f671fad12d271c67ed999401c86
guogege <784474077@qq.com>
2022-05-22 18:36:56 +0800

    第六章 整合ElasticSearch

---
+++ src/test/java/com/nowcoder/community/ElasticsearchTests.java
@@ -0,0 +1,125 @@
+package com.nowcoder.community;
+
+import com.alibaba.fastjson.JSONObject;
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import com.nowcoder.community.entity.DiscussPost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.naming.directory.SearchResult;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class ElasticsearchTests {
+    @Autowired
+    private DiscussPostMapper discussMapper;
+    
+    @Autowired
+    private DiscussPostRepository discussRepository;
+    
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
+
+    
+
+    @Test
+    public void testInsert() {
+        discussRepository.save(discussMapper.selectDiscussPostById(241));
+        discussRepository.save(discussMapper.selectDiscussPostById(242));
+        discussRepository.save(discussMapper.selectDiscussPostById(243));
+    }
+
+    @Test
+    public void testInsertList() {
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(101, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(102, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(103, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(111, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(112, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(131, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(132, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(133, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(134, 0, 100));
+    }
+    
+    @Test
+    public void testUpdate() {
+        DiscussPost post = discussMapper.selectDiscussPostById(231);
+        post.setContent("我是新人，使劲灌水.");
+        discussRepository.save(post);
+    }
+
+    @Test
+    public void testDelete() {
+        // discussRepository.deleteById(231);
+        discussRepository.deleteAll();
+    }
+    
+    @Test
+    public void testSearchByRepository() throws IOException {
+
+        SearchRequest searchRequest = new SearchRequest("discusspost");
+        //高亮
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("title");
+        highlightBuilder.field("content");
+        highlightBuilder.requireFieldMatch(false);
+        highlightBuilder.preTags("<span style='color:red'>");
+        highlightBuilder.postTags("</span>");
+        
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder()
+                .query(QueryBuilders.multiMatchQuery("互联网寒冬", "title", "content"))
+                .sort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .sort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .sort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .from(0)
+                .size(10)
+                .highlighter(highlightBuilder);
+        
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+
+        System.out.println(searchResponse.getHits().getTotalHits());
+        
+        
+        List<DiscussPost> list = new LinkedList<>();
+        long total = searchResponse.getHits().getTotalHits().value;
+        for (SearchHit hit : searchResponse.getHits().getHits()) {
+            DiscussPost discussPost = JSONObject.parseObject(hit.getSourceAsString(), DiscussPost.class);
+
+            // 处理高亮显示的结果
+            HighlightField titleField = hit.getHighlightFields().get("title");
+            if (titleField != null) {
+                discussPost.setTitle(titleField.getFragments()[0].toString());
+            }
+            HighlightField contentField = hit.getHighlightFields().get("content");
+            if (contentField != null) {
+                discussPost.setContent(contentField.getFragments()[0].toString());
+            }
+            System.out.println(discussPost);
+            list.add(discussPost);
+        }
+
+        
+    }
+}
