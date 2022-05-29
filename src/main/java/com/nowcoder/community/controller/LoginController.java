b65887849ddb6117173209e56f50721dd7c3d3ef
guogege <784474077@qq.com>
2022-05-29 11:40:03 +0800

    第七章 SpringSecurity权限控制

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import com.nowcoder.community.util.CommunityConstant;
 import org.springframework.ui.Model;
@@ -158,6 +159,7 @@ public class LoginController implements CommunityConstant{
     @RequestMapping(path = "/logout", method = RequestMethod.GET)
     public String logout(@CookieValue("ticket") String ticket) {
         userService.logout(ticket);
+        SecurityContextHolder.clearContext();
         return "redirect:/login";
     }
 }
